9d2f4679231ef54f3b61f2ec94dc33dc49b44515
你好2007 <2501482523@qq.com>
2022-05-15 12:36:08 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# common-3d-rendering-demonstration
+# [common-3d-rendering-demonstration](https://hai2007.github.io/common-3d-rendering-demonstration)
 常见3D绘制演示，为了演示方便，借助image3d来辅助提供基础设施。不过，这几乎没有什么难度，对于从没有接触过的人而言，也可以很快上手。
+
+## Issues
+使用的时候遇到任何问题或有好的建议，请点击进入[issue](https://github.com/hai2007/common-3d-rendering-demonstration/issues)！
+
+## How to use?
+
+开发的时候运行下列命令：
+
+```
+npm run dev
+```
+
+开发完毕需要发布的时候，请运行下列命令：
+
+```
+npm run build
+```
+
+当然，你也可以[在线访问](https://hai2007.github.io/common-3d-rendering-demonstration)哦~
+
+开源协议
+---------------------------------------
+[MIT](https://github.com/hai2007/common-3d-rendering-demonstration/blob/master/LICENSE)
+
+Copyright (c) 2022 [hai2007](https://hai2007.gitee.io/sweethome/) 走一步，再走一步。
---
+++ build/main@v0.1.0-bundle1.js
@@ -0,0 +1,44 @@
+
+/*************************** [bundle] ****************************/
+// Original file:./src/App/pages/begin/index.ts
+/*****************************************************************/
+window.__etcpack__bundleSrc__['27']=function(){
+    var __etcpack__scope_bundle__={};
+    var __etcpack__scope_args__;
+    var _dec, _class2;
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+__etcpack__scope_args__=window.__etcpack__getBundle('1');
+var Component=__etcpack__scope_args__.Component;
+
+__etcpack__scope_args__=window.__etcpack__getBundle('44');
+var template =__etcpack__scope_args__.default;
+
+
+var _class = (_dec = Component({
+  template: template,
+  styles: []
+}), _dec(_class2 = /*#__PURE__*/_createClass(function _class2() {
+  _classCallCheck(this, _class2);
+})) || _class2);
+
+__etcpack__scope_bundle__.default=_class;
+  
+    return __etcpack__scope_bundle__;
+}
+
+/*************************** [bundle] ****************************/
+// Original file:./src/App/pages/begin/index.html
+/*****************************************************************/
+window.__etcpack__bundleSrc__['44']=function(){
+    var __etcpack__scope_bundle__={};
+    var __etcpack__scope_args__;
+    __etcpack__scope_bundle__.default= "<div class='doc-view'>\n    <h2 id='fixed-top'>\n        写在前面\n    </h2>\n    <p>\n        接下来，我们将向大家说明一些常见的3D如何绘制，核心的内容是编程思路的分享，不涉及具体的绘图库。\n    </p>\n    <p>\n        在演示和说明的时候，虽然我们选择基于\n        <a href=\"https://hai2007.gitee.io/image3d\" target=\"_blank\" class='link'>image3D.js</a>\n        来作为依赖库，但由于其朴素的语法几乎和原生WebGL是一致的，因此我们认为这不是一个糟糕的选择。\n    </p>\n    <p class='warn'>\n        温馨提示：如果你有更好的建议，欢迎给我们\n        <a href=\"https://github.com/hai2007/common-3d-rendering-demonstration/issues\" target=\"_blank\"\n            class='link'>留言</a>~\n    </p>\n</div>\n"
+  
+    return __etcpack__scope_bundle__;
+}
